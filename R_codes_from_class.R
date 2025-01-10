d75c2eb22f9511e08437ce19044b6773b2216a91
dejenegizawkidane <105314121+dejenegizawkidane@users.noreply.github.com>
2025-01-10 14:21:07 +0100

    Add files via upload

---
+++ R_codes_from_class.R
@@ -0,0 +1,300 @@
+
+
+
+##############################
+#'  Forelesning 1 
+##############################
+#' `Objective`: review basic commands from `dplyr`
+#' 
+#' R makes use of the # or #' signs to add comments, 
+#' so that you and others can understand what the R code 
+#' is about.
+
+##########################################################
+##########################################################
+######
+#' `dplyr- Basic data manipulation`
+#' browseURL("https://r4ds.had.co.nz/transform.html")
+#' `filter()` - Pick observations (rows) by their values   
+#' `select()` -  Pick variables (columns) by their names  
+#' `mutate()` - Create new variables (columns) with functions of existing variables  
+#' `arrange()`-  Reorder rows using  
+#' `summarise()` - Collapse many values down to a single summary 
+#' `rename()`  - Rename columns (variables):
+#' `group_by()` - Group rows by columns (variables)
+#' 
+##########################################################
+#############################################################
+########
+
+
+rm (list = ls())
+library(tidyverse)
+
+#' In this exercise, we utilize the gapminder dataset 
+#' provided by the gapminder R package.
+
+#install.packages("gapminder")
+library(gapminder) 
+
+data("gapminder")
+
+head(gapminder)
+tail(gapminder)
+
+View(gapminder)
+
+?gapminder
+
+str(gapminder)
+
+# Some Questions:
+
+
+
+
+#' Task 1. From the gapminder dataset, select country, year & pop 
+
+gapminder %>% select(country,year,pop)
+
+gapminder %>% 
+  select(starts_with("c"))
+
+gapminder %>% 
+  select(ends_with(c("try","Exp")))
+
+
+#' Task 2. Filter the data from Norway, Sweden, or Denmark 
+#' before and including 1970. And select the variables
+#' country, year, gdpPercap. And rename gdpPercap by gdpc
+
+gapminder %>% 
+  filter(year >=1970) %>% 
+  filter(country %in% c("Norway","Sweden","Denmark")) %>% 
+  select(country,year,gdpPercap) %>% 
+  rename(gdpc= gdpPercap)
+
+
+#' Task 3. Following Q2. Arrange rows according to 
+#' ascending and descending order of "gdpc" using the function 
+#' arrange()  
+
+# Ascending order 
+gapminder %>% 
+  filter(year >=1970) %>% 
+  filter(country %in% c("Norway","Sweden","Denmark")) %>% 
+  select(country,year,gdpPercap) %>% 
+  rename(gdpc= gdpPercap) %>% 
+  arrange(gdpc)
+
+# descending order 
+gapminder %>% 
+  filter(year >=1970) %>% 
+  filter(country %in% c("Norway","Sweden","Denmark")) %>% 
+  select(country,year,gdpPercap) %>% 
+  rename(gdpc= gdpPercap) %>% 
+  arrange(desc(gdpc))
+
+#' Task 4.  Arrange rows according to ascending order 
+#' of "gdpc", within each year. 
+
+gapminder %>% 
+  filter(year >=1970) %>% 
+  filter(country %in% c("Norway","Sweden","Denmark")) %>% 
+  select(country,year,gdpPercap) %>% 
+  rename(gdpc= gdpPercap) %>% 
+  arrange(year,gdpc) 
+
+
+#' Task 5. Create a new variable that is gdp from gdpPercap & pop and obtain data only from Norway 
+
+gapminder %>% 
+  filter(country =="Norway") %>% 
+  mutate(gdp = gdpPercap*pop)
+
+
+#' Task 6. Data from Norway. 
+#' Create a new variable called "gdpNOK" that is GDP per per billion NOK (1 000 000 000 NOK) 
+#' (1 USD=9 NOK)
+
+gapminder %>% 
+  filter(country =="Norway") %>% 
+  mutate(gdp = gdpPercap*pop) %>% 
+  mutate(gdpNOK = gdp*9/1e9)
+
+#' Task 7. Use mutate and ifelse to Categorise "gdpNOK" into 3 categories,
+#'  (i.e., less than or equal to 999, between 1000 and 1999, and greater than 2000).
+
+gapminder %>% 
+  filter(country =="Norway") %>% 
+  mutate(gdp = gdpPercap*pop) %>% 
+  mutate(gdpNOK = gdp*9/1e9) %>% 
+  mutate(gdpcat = ifelse(gdpNOK<=999,"less than a billion",
+                         ifelse(gdpNOK >=1000 & gdpNOK <=1999,"between a billion and two billions",
+                                ifelse(gdpNOK >2000,"larger than two bilions","NA"))))
+
+# or 
+gapminder %>% 
+  filter(country =="Norway") %>% 
+  mutate(gdp = gdpPercap*pop) %>% 
+  mutate(gdpNOK = gdp*9/1e9) %>% 
+  mutate(gdpcat = ifelse(gdpNOK<=999,"less than a billion",
+                         ifelse(between(gdpNOK,1000, 1999),"between a billion and two billions",
+                                ifelse(gdpNOK >2000,"larger than two bilions","NA"))))
+
+
+#' Task 8. Calculate the average lifExp of all three Nordic countries
+#' (i.e., Norway, Sweden, Denmark).
+#' And assign a name to the calculated mean lifeEXP, call it avlfexp
+
+gapminder %>% 
+  filter(year >=1970) %>% 
+  filter(country %in% c("Norway","Sweden","Denmark")) %>% 
+  summarize(avlfexp = mean(lifeExp, na.rm= TRUE))
+
+
+
+#' Task 9 Calculate the average lifExp of the three countries,per country. 
+#' (i.e., average per country, name variable )
+
+gapminder %>% 
+  filter(year >=1970) %>% 
+  filter(country %in% c("Norway","Sweden","Denmark")) %>% 
+  group_by(country) %>% 
+  summarize(avlfexp = mean(lifeExp, na.rm= TRUE))
+
+
+# Task: how many unique countries does the gapminder data set contain, by continent?
+gapminder %>%
+  group_by(continent) %>%
+  summarize(n_obs = n(), 
+            n_countries = n_distinct(country))
+
+
+
+#' Task 10. Calculate mean life expectancy per country 
+#' (i.e., for every country in the gapminder data set)
+
+gapminder %>% 
+  group_by(country) %>% 
+  summarise(avlfexp = mean(lifeExp, na.rm = TRUE))
+
+#' Task 11. Calculate mean life expectancy per continent.
+gapminder %>% 
+  group_by(continent) %>% 
+  summarise(avlfexp = mean(lifeExp, na.rm = TRUE))
+
+#' Task 12. calculate mean life expectancy by continent & country
+gapminder %>% 
+  group_by(continent,country) %>% 
+  summarise(avlfexp = mean(lifeExp, na.rm = TRUE))
+
+#' Task 13. Calculate mean life expectancy by continent & add min and max lifeExp
+
+gapminder %>% 
+  group_by(continent,country) %>% 
+  summarise(avlfexp = mean(lifeExp, na.rm = TRUE),
+            min.lfexp = min(lifeExp),
+            max.lfexp = max(lifeExp))
+
+
+#' Visualization
+
+# Task 14. Scatter plot of gdpPercap vs lifeExp  
+
+gapminder %>% 
+  ggplot(aes(x=gdpPercap,y= lifeExp))+
+  geom_point(alpha=0.5)+
+  ggtitle("Life Expectancy and GDP per capita")
+
+# Task 15. Scatter plot of gdpPercap vs lifeExp by Continent
+gapminder %>% 
+  ggplot(aes(x=gdpPercap,y= lifeExp, colour = continent, shape = continent))+
+  geom_point(alpha=0.5)+
+  ggtitle("Life Expectancy and GDP per capita")
+
+
+#' Task 16. scatter plot per continent, separate graphs for each continent 
+gapminder %>% 
+  ggplot(aes(x=gdpPercap,y= lifeExp, colour = continent, shape = continent))+
+  geom_point(alpha=0.5)+
+  facet_wrap(~continent,ncol=3)
+ggtitle("Life Expectancy and GDP per capita")
+
+
+
+#' Task 17.  histogram plot of lifeExp per continent 
+
+gapminder %>% 
+  ggplot(aes(x= lifeExp))+
+  geom_histogram(binwidth = 2)+
+  facet_wrap(~continent,ncol=3)+
+  ggtitle("Life Expectancy by continent")
+
+
+#' Task 18. Scatter plot of gdpPercap vs lifeExp for the 3 nordic countries
+
+gapminder %>% 
+  filter(year >=1970) %>% 
+  filter(country %in% c("Norway","Sweden","Denmark")) %>% 
+  ggplot(aes(x= gdpPercap, y=lifeExp, colour = country))+
+  geom_point(alpha=0.5)
+
+gapminder %>% 
+  filter(year >=1970) %>% 
+  filter(country %in% c("Norway","Sweden","Denmark")) %>% 
+  ggplot(aes(x= gdpPercap, y=lifeExp, colour = country))+
+  geom_point(alpha=0.5)+
+  geom_text(aes(label = as.character(year),hjust = 0, vjust = 1))+
+  geom_line()
+
+
+
+
+#' Data structure
+
+#' the same data can be represented/arranged in multiple ways. 
+#' But not all are equally easy to use.
+#' It is easy to work with tidy dataset
+
+
+#' Task 19. Filter the data from Norway, Sweden, or Denmark 
+#' before and including 1970. And select the variables
+#' country, year, gdpPercap. And rename gdpPercap by gdpc
+
+
+#' Task 20. Change the above data to wider format
+
+
+#' Task 21: chnage the data back to long format
+
+
+#' Merging/joining dataframes  
+
+
+
+
+
+
+#' Task 22. Find the correlation between lifeExp and gdpPercap for all countries
+#' and sort in ascending  order. And also in descending order 
+
+
+
+#' Task 23:  From the previous task, take the top two and bottom two countries.
+#' And plot lifeExp vs gdpPercap per country 
+
+
+
+#' Task 24: calculate the percentage and logaritmic growth of gdp for Norway 
+
+
+#' Task 25: calculate the logaritmic growth in gdp for all countries
+
+
+#' Task 26: what country has the highest and lowest average logaritmic growth in gdp?  
+
+#' lowest
+
+
+#' highest
