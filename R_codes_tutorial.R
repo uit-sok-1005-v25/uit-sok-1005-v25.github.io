e5a6b99ba22e9f2928a30ce31ae45f939bddb68a
dejenegizawkidane <105314121+dejenegizawkidane@users.noreply.github.com>
2025-01-10 14:51:09 +0100

    Update R_codes_tutorial.R

--- R_codes_tutorial.R
+++ R_codes_tutorial.R
@@ -2,8 +2,9 @@
 # R tutorials 
 
 library(r4ds.tutorials)
-learnr::run_tutorial(package = "r4ds.tutorials")
+learnr::run_tutorial(package = "r4ds.tutorials") # it takes some minutes to run 
 
-learnr::run_tutorial("00-introduction",package = "r4ds.tutorials")
+# For example if you want to look at the tutorial on Introduction to R
+learnr::run_tutorial("00-introduction",package = "r4ds.tutorials") 
 
-
+# yo can replace "00-introduction" in the above code to look at any other tutorial 
